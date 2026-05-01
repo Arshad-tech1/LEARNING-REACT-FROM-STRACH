38e82ac904147ef0ecafc8b27e58e38c0b8ad1f1
Mohd_Arshad <mohdarshad12575@gmail.com>
2026-05-01 19:02:29 +0530

    Refactor form to use controlled components
    Updated form to use controlled components for name and email inputs. Added alert on form submission.

--- my-react-app/src/components/form.jsx
+++ my-react-app/src/components/form.jsx
@@ -1,33 +1,46 @@
-import React from 'react';
+
+import React,{useState} from 'react';
 
 function Form() {
+    const [name,setName] = useState("")
+    const [email,setEmail] = useState("")
 
     const onSubmit= (e) => {
         e.preventDefault();
-        console.log('Form submitted!');
+        console.log(name)
+        console.log(email)
+        alert('Form submitted!');
     }
   return (
     <>
-        <form className="m-4 border border-gray-400 p-6 w-80  mt-10 rounded-lg shadow-md">
-        <input
-            type="text"
-            placeholder="Enter name"
-            className="w-full p-2 mb-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-        />
+        <form onSubmit={(e) => { onSubmit(e)}} className="m-4 border border-gray-400 p-6 w-80  mt-10 rounded-lg shadow-md">
+            <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                name="name"
+                type="text"
+                placeholder="Enter name"
+                className="w-full p-2 mb-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+                required
+            />
 
-        <input
-            type="email"
-            placeholder="Enter email"
-            className="w-full p-2 mb-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-        />
+            <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                name="email"
+                type="email"
+                placeholder="Enter email"
+                className="w-full p-2 mb-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+                required
+            />
 
-        <button
-            onSubmit={(e) => onSubmit(e)}
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
-        >
-            Submit
-        </button>
+
+            <button
+                type="submit"
+                className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+            >
+                Submit
+            </button>
         </form>
     </>
   );
